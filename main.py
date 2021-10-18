5e770fea59a26266592ca553c60132e495764040
AmarildoJr <>
2021-10-18 17:50:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord 
+from discord.ext import commands
+import random
+
+client = commands.Bot(command_prefix = '%', case_insensitive = True)
+
+@client.event
+async def on_ready():
+  print("Entramos com {0.user}".format(client))
+
+@client.command()
+async def ola(ctx):
+  await ctx.send(f"Olá, {ctx.author}")
+
+@client.command()
+async def aleatorio(ctx, numero1, numero2):
+  n = random.randint(int(numero1), int(numero2))
+  await ctx.send(f"O numero aletório escolhido entre {numero1} e {numero2} foi {n}.")
+
+client.run("ODk5NzA3NTA0NzE0NDY1Mzcx.YW2r6A.5QgMynywlAqnE6E3Yryl03ui3W0")
