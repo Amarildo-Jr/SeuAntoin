6dcf63d0b2b1c7f1e8ebb294da53a13bfe4347ff
Amarildo Junior <77510717+Amarildo-Jr@users.noreply.github.com>
2021-11-05 05:18:46 +0000

    Adição de comandos: mover usuários de canal de voz, teste e comentarios no codigo

--- main.py
+++ main.py
@@ -1,21 +1,49 @@
-import os
 import discord 
+import os
+from dotenv import load_dotenv
 from discord.ext import commands
 import random
 
-client = commands.Bot(command_prefix = '%', case_insensitive = True)
+load_dotenv()
 
-@client.event
+bot = commands.Bot(command_prefix = '$', case_insensitive = True)
+
+#avisar no console que o bot ta on
+@bot.event
 async def on_ready():
-  print("Entramos com {0.user}".format(client))
+  print("{0.user.name} tá on!".format(bot))
 
-@client.command()
+#bot diz ola a quem o chama
+@bot.command()
 async def ola(ctx):
-  await ctx.send(f"Olá, {ctx.author}")
+  await ctx.send(f"Ola, {ctx.author.name}")
 
-@client.command()
+#teste pra saber se ta ativo
+@bot.command()
+async def teste(ctx):
+  await ctx.send("FALAAAA!")
+
+#mover certo membro pra determinado canal
+@bot.command()
+async def move(ctx, member : discord.Member, channel : discord.VoiceChannel):
+  await member.move_to(channel)
+
+#mover todos os membros de um canal para outro
+@bot.command()
+async def move_all(ctx, channel_from : discord.VoiceChannel, channel_to : discord.VoiceChannel):
+  print(channel_from.members)
+  for member in channel_from.members:
+    await member.move_to(channel_to)
+
+#escolher numero aleatorio entre dois numeros fornecidos
+@bot.command()
 async def aleatorio(ctx, numero1, numero2):
   n = random.randint(int(numero1), int(numero2))
   await ctx.send(f"O numero aletório escolhido entre {numero1} e {numero2} foi {n}.")
 
-client.run(os.environ['TOKEN'])
+#@client.command()
+#async def joseph
+
+bot.run(os.environ['TOKEN'])
+
+#client.run(DISCORD_TOKEN)
